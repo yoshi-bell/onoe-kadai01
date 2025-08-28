32ff29ad6bce59b3fb1931a4eb88b450f838ad2c
onoe yoshiyuki <onoe.sanpo@gmail.com>
2025-08-28 12:59:21 +0900

    不要ファイル暫定削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
 
@@ -19,8 +19,16 @@ Route::post('/back', [ContactController::class, 'back']);
 
 // 管理者向けのルート
 Route::middleware('auth')->group(function () {
-    // AuthControllerのadminルートを削除し、AdminControllerに統一
     Route::get('/admin', [AdminController::class, 'index'])->name('admin');
     Route::get('/admin/search', [AdminController::class, 'search'])->name('admin.search');
     Route::post('/admin/delete/{contact_id}', [AdminController::class, 'delete'])->name('admin.delete');
 });
+
+//ログアウト時ログインページに移動
+Route::post(
+    '/logout',
+    function () {
+        Auth::logout();
+        return redirect('/login');
+    }
+);
